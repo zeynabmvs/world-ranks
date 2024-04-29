a3808c46859ee3341e80623214e8a0677d980025
Zeynab Movasaqpoor <zeynab.mvs@gmail.com>
2024-04-29 08:45:51 +0330

    add header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import Logo from "/images/Logo.svg";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div
+      id="header"
+      className="h-[240px] md:h-[300px] bg-[url('/images/hero-image-wr.jpg')] bg-no-repeat bg-cover flex items-center"
+    >
+      <Link to="/" className="mx-auto">
+        <img src={Logo} alt="World Ranks" />
+      </Link>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
