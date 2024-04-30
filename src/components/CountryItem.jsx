6d943581748c3e7bf0aae4c8f49405c305e8882f
Zeynab Movasaqpoor <zeynab.mvs@gmail.com>
2024-04-30 11:00:27 +0330

    add ?. to safe access properties

--- src/components/CountryItem.jsx
+++ src/components/CountryItem.jsx
@@ -4,9 +4,9 @@ const CountryItem = ({country}) => {
   return (
     <tr className="text-base text-slate-100 text-center relative hover:bg-slate-300">
     <td className="py-3"> <img
-          src={country.flags.svg}
+          src={country?.flags?.svg}
           className="w-[50px] h-[38px] mx-auto rounded-lg"></img>
-      <Link to={`/country/${country.name.common}`}
+      <Link to={`/country/${country?.name?.common}`}
         className="absolute inset-0">
         <span className="sr-only">
           View more information about {country.name.common}
@@ -15,13 +15,13 @@ const CountryItem = ({country}) => {
     </td>
 
     <td className="py-3">
-      {country.name.common}
+      {country?.name?.common}
     </td>
     <td className="py-3">
-      {country.population.toLocaleString()}
+      {country?.population.toLocaleString()}
     </td>
     <td className="py-3">
-      {country.area.toLocaleString()}
+      {country?.area.toLocaleString()}
     </td>
     {/* <td className="py-3">
       {country.independent && '**'}
@@ -30,7 +30,7 @@ const CountryItem = ({country}) => {
       {country.unMember && '**'}
     </td> */}
     <td className="py-3 hidden md:block">
-      {country.region}
+      {country?.region}
     </td>
   </tr>
   )
